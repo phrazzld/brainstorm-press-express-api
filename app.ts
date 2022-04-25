d0016ea55883fea42228cac0ea270e012ba9f71b
Phaedrus Raznikov <praznikov@cerego.com>
2022-04-25 07:57:30 -0400

    Change CORS policy

--- app.ts
+++ app.ts
@@ -12,12 +12,12 @@ const SocketEvents = {
 
 // Create Express server
 const { app } = expressWs(express());
-app.use(cors({ origin: process.env.BASE_URL }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
